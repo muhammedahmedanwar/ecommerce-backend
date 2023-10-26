7b76ff29a6e75e5db7ba9657e40033f063220234
muhammedahmedanwar <mgego0581@gmail.com>
2023-10-26 12:22:45 +0300

    changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
 // Routes
 const mountRoutes = require("./routes");
-
+const { webhookCheckout } = require("./services/orderService");
 // Connect with db
 dbConnection();
 
@@ -26,6 +26,13 @@ app.options("*", cors());
 
 // compress all responses
 app.use(compression());
+
+// checkout webhook
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  webhookCheckout
+);
 
 // Middlewares
 app.use(express.json());
